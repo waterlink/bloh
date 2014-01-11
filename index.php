ff6115702aedc2aac7cef19afea4d8cbedd64777
Alexey Fedorov <waterlink000@gmail.com>
2014-01-11 16:18:31 +0200

    Added hello world app and nginx config

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Bloh</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="index.js"></script>
+  </head>
+  <body>
+    <? $name = 'alex' ?>
+    <h1>Hello <?= $name ?>!</h1>
+  </body>
+</html>
