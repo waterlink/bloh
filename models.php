c19b3e90d0aa99cf342a1b88d9ba5cf51bf40680
Adamets Andrey <andrey.adamets@gmail.com>
2014-01-14 15:49:43 +0200

    Rewrite save function in Post model, added id and updated_at properties

--- models.php
+++ models.php
@@ -4,7 +4,7 @@ require_once 'connect.php';
 
 class Post {
 
-  public $title, $content, $created_at;
+  public $id, $title, $content, $created_at;
 
   // Find one, find all, post one
 
@@ -21,8 +21,29 @@ class Post {
     echo "Suck\n";
   }
 
-  function save ($content) {
-
+  function save () {
+    global $db;
+    if (is_null($this->id)) {
+      $this->created_at = time();
+      $this->updated_at = $this->created_at;
+      $sql = 'insert into post (id, title, content, created_at, updated_at) values (:id, :title, :content, :created_at, :created_at)';
+      $q = $db->prepare($sql);
+      $this->id = $db->query("select nextval('bloh_post_id_seq')")->fetch();
+      $q->execute(array(':id'=>$this->id,
+                        ':title'=>$this->title,
+                        ':content'=>$this->content,
+                        ':created_at'=>$this->created_at));
+    } else {
+      $this->updated_at = time();
+      $sql = "update post set title = :title, content = :content, updated_at = :updated_at where id=:id";
+      $q = $db->prepare($sql);
+      $q->execute(array(":title"=>$this->title,
+                        ":content"=>$this->content,
+                        ":updated_at"=>$this->updated_at,
+                        ":id"=>$this->id));
+    }
   }
 
 }
+$post = new Post(array('title'=>"Bga", 'content'=>"first post"));
+$post->save();
