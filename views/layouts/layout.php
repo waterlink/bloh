da07c8afc3abd79a364b88130e7a36d694729953
Adamets Andrey <andrey.adamets@gmail.com>
2014-01-20 18:15:08 +0200

    Created custom color with green and red colors

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -3,13 +3,18 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css" type="text/css">
-    <script src="index.js"></script>
+    <link rel="stylesheet" href="/css/style.css" type="text/css">
+    <script src="/js/index.js"></script>
     <title><?= $title ?> - Bloh!</title>
   </head>
   <body>
-    <?
-      require_once $_SERVER['DOCUMENT_ROOT']."/".$view_name.".php";
-    ?>
+  <div class="main-container">
+    <h1><?= $title ?></h1>
+    <div class="container">
+      <?
+        require_once $_SERVER['DOCUMENT_ROOT']."/".$view_name.".php";
+      ?>
+    </div>
+  </div>
   </body>
 </html>
--- views/posts/post.php
+++ views/posts/post.php
@@ -5,11 +5,18 @@ require_once $_SERVER['DOCUMENT_ROOT']."/models/post.php";
   <form action="/logic/posts/destroy.php?id=<?=$post->id?>" method="post">
     <input type="hidden" name="method" value="delete">
     <article>
-      <h1><?= $post->title ?></h1>
-      <div><?= $post->content ?></div>
-      <div><i>updated at <?= $post->updated_at?></i></div>
-      <button>Remove Post</button>
-      <a href="/logic/posts/edit.php?id=<?= $post->id?>">Edit post</a>
+
+      <div class="row">
+        <h1 class="pool-left"><?= $post->title ?></h1>
+        <div class="pool-right"><i>updated at <?= $post->updated_at?></i></div>
+      </div>
+      <div class="row">
+        <div class="post-content"><?= $post->content ?></div>
+        <div class="row">
+          <button>Remove Post</button>
+          <a class="btn custom ok" role="button" href="/logic/posts/edit.php?id=<?= $post->id?>">Edit post</a>
+        </div>
+      </div>
     </article>
   </form>
   <a href="<?=Post::get_index_url()?>">All posts</a>
