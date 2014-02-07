bbdbea6a43233207e24f73b2b82e39cf70a60aec
Adamets Andrey <andrey.adamets@gmail.com>
2014-02-07 18:11:52 +0200

    Added displaying comments

--- scripts/post.sql
+++ scripts/post.sql
@@ -3,7 +3,6 @@ create table post (
   id integer primary key default nextval('bloh_post_id_seq'),
   title varchar(50),
   content varchar(4000),
-  created_at timestamp
+  created_at timestamp,
+  updated_at timestamp
 );
-
-alter table post add updated_at timestamp;
