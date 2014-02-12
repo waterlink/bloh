df7762c65059cdfa1507686f99e2b71356901fa5
Adamets Andrey <andrey.adamets@gmail.com>
2014-02-12 18:36:48 +0200

    Changing work with sql

--- scripts/user/create.sql
+++ scripts/user/create.sql
@@ -1,6 +1,6 @@
 create sequence bloh_user_id_seq;
-create table user (
+create table bloher (
   id integer primary key default nextval('bloh_user_id_seq'),
   email varchar(50),
-  password (50)
+  password varchar (50)
 );
