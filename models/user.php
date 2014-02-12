cc8171d7b0a453d21aa93253964c832524362ea3
Adamets Andrey <andrey.adamets@gmail.com>
2014-02-12 10:24:52 +0200

    Renamed .sql files and moved to folders

--- models/user.php
+++ models/user.php
@@ -3,6 +3,6 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/models/base.php';
 class User extends Base {
   public static $table_name = 'user';
   public static $sequence_id = 'bloh_user_id_seq';
-  public static $columns = array('id', 'name', 'soname', 'login', 'password');
+  public static $columns = array('id', 'email', 'password');
 }
 ?>
