0d42c9ff86906c16967076c2af79ce53b3f2281d
Adamets Andrey <andrey.adamets@gmail.com>
2014-02-28 11:13:16 +0200

    Removed unuseful comments from authorization and log_out logic

--- logic/users/authorization.php
+++ logic/users/authorization.php
@@ -17,27 +17,3 @@ if (isset($user_id) and isset($random_hash)){
     require_once $_SERVER['DOCUMENT_ROOT'].'/views/users/signed.php';
   }
 }
-//   // require_once $_SERVER['DOCUMENT_ROOT']."/views/users/sign_in.php";
-// }
-// else {
-//   $user = null;
-// }
-  // require_once $_SERVER['DOCUMENT_ROOT']."/views/users/index.php";
-// $user_id = $_COOKIE['user_id'];
-// $random_hash = $_COOKIE['random_hash'];
-// $view_name = "/views/users/sign_in";
-// // $view_name = "/views/users/authorization";
-// $email = $_POST['email'];
-// $password = $_POST['password'];
-// $user_id = htmlspecialchars($_COOKIE["user_id"]);
-// $random_hash = htmlspecialchars($_COOKIE["random_hash"]);
-// if (isset($email) and isset($password)){
-//   $user = new User(array("email"=>$email, "password"=>$password));
-//   if($user->validate()){
-//     $session = new Session(array("user_id"=>$user->id));
-//     $session->save();
-//     // header("Location:"."/index.php");
-//   }
-// }
-
-// require $_SERVER['DOCUMENT_ROOT']."/views/layouts/layout.php";
--- logic/users/log_out.php
+++ logic/users/log_out.php
@@ -4,6 +4,3 @@ $session_id = $_POST['session_id'];
 $session = Session::find($session_id);
 $session->destroy();
 header("Location:"."/index.php");
-// $session->destroy();
-// $user = null;
-// header("Location:"."/index.php");
